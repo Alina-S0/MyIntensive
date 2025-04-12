3d20202a265d0e7a6c793677224ad3687d52a1ff
Alina-S0 <a.selimanova@mail.ru>
2025-04-12 20:39:01 +0300

    Add files via upload

---
+++ Animals.java
@@ -0,0 +1,173 @@
+public class Animals {
+        public static void main(String[] args) {
+            Dog dog1 = new Dog("Вася");
+            // Dog dog2 = new Dog("");
+
+            Cat cat1 = new Cat("Лили");
+            Cat cat2 = new Cat("Граф");
+          //  Cat cat3 = new Cat("Рыжик");
+
+            System.out.println("Животных всего: " + Animal.getAnimalCount());
+            System.out.println("Собак всего: " + Dog.getDogCount());
+            System.out.println("Котов всего: " + Cat.getCatCount());
+
+            dog1.run(450);
+            dog1.swim(9);
+
+            //dog2.run(600);
+           // dog2.swim(15);
+
+            cat1.run(100);
+            cat1.swim(10);
+
+            Foods foods = new Foods();
+            Cat[] cats = {cat1, cat2};
+
+            for (Cat cat : cats) {
+                cat.eat(foods, 9);
+                System.out.println(cat.getName() + " сыт: " + cat.isFull());
+            }
+            System.out.println("Остаток еды в миске: " + foods.getFood() + " ед.");
+
+            foods.addFood(17);
+            System.out.println("Еда добавлена, в миске теперь: " + foods.getFood() + " ед.");
+
+            for (Cat cat : cats) {
+                if (!cat.isFull()) {
+                    cat.eat(foods, 10);
+                    System.out.println(cat.getName() + " сыт: " + cat.isFull());
+                }
+            }
+            System.out.println("Осталось еды: " + foods.getFood() + " ед.");
+
+
+        }
+
+    }
+
+    abstract class Animal {
+        protected String name;
+        protected static int animalCount = 0;
+
+        public Animal(String name) {
+            this.name = name;
+            animalCount++;
+        }
+
+        public abstract void run(int distance);
+        public abstract void swim(int distance);
+
+        public static int getAnimalCount() {
+            return animalCount;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    class Dog extends Animal {
+        private static final int MAX_RUN_DISTANCE = 500;
+        private static final int MAX_SWIM_DISTANCE = 10;
+        private static int dogCount = 0;
+
+        public Dog(String name) {
+            super(name);
+            dogCount++;
+        }
+
+        public void run(int distance) {
+            if (distance <= MAX_RUN_DISTANCE) {
+                System.out.println(name + " пробежал " + distance);
+            } else {
+                System.out.println(name + " не смог пробежать " + distance);
+            }
+        }
+
+        public void swim(int distance) {
+            if (distance <= MAX_SWIM_DISTANCE) {
+                System.out.println(name + " проплыл " + distance);
+            } else {
+                System.out.println(name + " не смог проплыть " + distance);
+            }
+        }
+
+        public static int getDogCount() {
+            return dogCount;
+        }
+    }
+
+    class Cat extends Animal {
+        private static final int MAX_RUN_DISTANCE = 200;
+        private static int catCount = 0;
+        private boolean isFull;
+
+        public Cat(String name) {
+            super(name);
+            catCount++;
+            this.isFull = false;
+        }
+
+        public void run(int distance) {
+            if (distance <= MAX_RUN_DISTANCE) {
+                System.out.println(name + " пробежал(а) " + distance + " м.");
+            } else {
+                System.out.println(name + " не смог(ла) пробежать " + distance + " м.");
+            }
+        }
+
+        public void swim(int distance) {
+            System.out.println(name + " не умеет плавать");
+        }
+
+        public void eat(Foods foods, int amount) {
+            if (foods.decreaseFood(amount)) {
+                this.isFull = true;
+                System.out.println(name + " поел(a) и теперь сыт(a)");
+            } else {
+                System.out.println(name + " не смог(ла) поесть - недостаточно еды в миске");
+            }
+        }
+
+        public boolean isFull() {
+            return isFull;
+        }
+
+        public static int getCatCount() {
+            return catCount;
+        }
+    }
+
+    class Foods {
+        private int food;
+
+        public void foods(int foods) {
+            this.food = foods;
+        }
+
+        public boolean decreaseFood(int amount) {
+            if (amount <= 0) {
+                System.out.println("Eды должно быть больше нуля");
+                return false;
+            }
+            if (food >= amount) {
+                food -= amount;
+                return true;
+            }
+            System.out.println("Недостаточно еды - oсталось: " + food + " ед.");
+            return false;
+        }
+
+        public void addFood(int amount) {
+            if (amount > 0) {
+                food += amount;
+                System.out.println("Добавили " + amount + " еды в миску");
+            } else {
+                System.out.println("Количество добавляемой еды должно быть больше нуля!");
+            }
+        }
+
+        public int getFood() {
+            return food;
+        }
+    }
---
+++ Shape.java
@@ -0,0 +1,132 @@
+interface Shape {
+    public static void main(String[] args) {
+
+        System.out.println("Фигуры");
+        Shape circle = new Circle(5, "White", "Black");
+        Shape rectangle = new Rectangle(3, 9, "Blue", "Ping");
+        Shape triangle = new Triangle(4, 4, 5, "Yellow", "Blue");
+
+
+        System.out.println("Круг:");
+        circle.printCharacteristics();
+        System.out.println("\nПрямоугольник:");
+        rectangle.printCharacteristics();
+        System.out.println("\nТреугольник:");
+        triangle.printCharacteristics();
+    }
+    double getPerimeter();
+    double getArea();
+    String getFillColor();
+    String getBorderColor();
+
+    default void printCharacteristics() {
+        System.out.println("Периметр: " + getPerimeter());
+        System.out.println("Площадь: " + getArea());
+        System.out.println("Фон: " + getFillColor());
+        System.out.println("Граница: " + getBorderColor());
+    }
+}
+
+class Circle implements Shape {
+    private double radius;
+    private String fillColor;
+    private String borderColor;
+
+    public Circle(double radius, String fillColor, String borderColor) {
+        this.radius = radius;
+        this.fillColor = fillColor;
+        this.borderColor = borderColor;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public String getFillColor() {
+        return fillColor;
+    }
+
+    @Override
+    public String getBorderColor() {
+        return borderColor;
+    }
+}
+
+class Rectangle implements Shape {
+    private double width;
+    private double height;
+    private String fillColor;
+    private String borderColor;
+
+    public Rectangle(double width, double height, String fillColor, String borderColor) {
+        this.width = width;
+        this.height = height;
+        this.fillColor = fillColor;
+        this.borderColor = borderColor;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+
+    @Override
+    public String getFillColor() {
+        return fillColor;
+    }
+
+    @Override
+    public String getBorderColor() {
+        return borderColor;
+    }
+}
+
+class Triangle implements Shape {
+    private double sideA;
+    private double sideB;
+    private double sideC;
+    private String fillColor;
+    private String borderColor;
+
+    public Triangle(double sideA, double sideB, double sideC, String fillColor, String borderColor) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+        this.fillColor = fillColor;
+        this.borderColor = borderColor;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return sideA + sideB + sideC;
+    }
+
+    @Override
+    public double getArea() {
+        double s = getPerimeter() / 2.0;
+        return Math.sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+
+    }
+
+    @Override
+    public String getFillColor() {
+        return fillColor;
+    }
+
+    @Override
+    public String getBorderColor() {
+        return borderColor;
+    }
+}
